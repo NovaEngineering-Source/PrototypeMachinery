df94c51b777d955e9cebb605e1feda1732e5ff5c
Hikari_Nova <3044344887@qq.com>
2025-12-07 21:37:11 +0800

    - Update mod information and descriptions.

--- src/main/kotlin/PrototypeMachinery.kt
+++ src/main/kotlin/PrototypeMachinery.kt
@@ -4,7 +4,7 @@ import net.minecraftforge.fml.common.Mod
 
 @Mod(
     modid = Tags.MOD_ID,
-    name = Tags.MOD_NAME,
+    name = "Prototype Machinery",
     version = Tags.VERSION,
     acceptedMinecraftVersions = "[1.12.2]",
     dependencies = "required-after:forge@[14.23.5.2847,);",
